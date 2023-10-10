1b9080bc13bba8d8efdcd35635bc356d20c19c44
SergioMorales12 <smorcor744a@g.educaand.es>
2023-10-10 19:49:30 +0200

    nuevo

--- ejercicio1.29.py
+++ ejercicio1.29.py
@@ -1,29 +1,29 @@
+"""
+Inicio
+    Escribe "Introduce tu nombre: "
+    Lee nombre
+    Escribe "Introduce tu edad: "
+    Lee edad
 
-#Inicio
-#Escribe "Introduce tu nombre: "
-#Escribe "Introduce tu edad: "
+    Si (nombre == "") hacer 
+        Lee "Desconocido"
+        nombre = "Desconocido"
 
-#Mientras (nombre esta vacio) hacer 
-#   Lee "Desconocido"
-#   nombre = 0
-
-#Mientras (edad <= 125) y  (edad >= 0) hacer 
-#   Escribe "Introduce tu edad: "
+    Mientras (edad >= 125) y (edad <= 0) hacer 
+        Escribe "Introduce tu edad: "
+        lee edad
+    
 
 
-#Si (125 - edad >= 0) entonces
-#años = 125 - edad
-
-#Fin
+Fin
+"""
 nombre = input("Introduce tu nombre: ") 
 edad = int(input("Introduce tu edad: ")) 
+if nombre == "" :
+    nombre = "Desconocido"
 
-while edad >= 125 and  edad >= 0 : 
+while edad >= 125 and  edad <= 0 : 
     edad = input("Introduce tu edad: ") 
-
-
-if 125 - edad >= 0 : 
-    años = 125 - edad 
-    print(f"Te llamas {nombre} y tienes {edad} años, te quedan aún {años} años por cumplir") 
-else :
-    print("Desconocido")
+    edad = int(input("Introduce la edad: "))
+diferencia = 125 - edad
+print(f"Te llamas {nombre} y tienes {edad} años, te quedan aún {diferencia} años por cumplir")
