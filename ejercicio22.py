73125ded4f781fdca73159d9bad33040c7ed6344
smorcor <146772120+smorcor@users.noreply.github.com>
2023-10-10 12:40:58 +0200

    Update ejercicio22.py

--- ejercicio22.py
+++ ejercicio22.py
@@ -1,8 +1,6 @@
 frase = input("Escribe una frase: ")
 vocal = input("Escribe una vocal: ")
+vocal = vocal.lower()
+frasem = frase.replace(vocal, vocal.upper())
+print("Frase con la vocal en mayúscula:", frasem)
 
-if len(vocal) == 1 and vocal in "aeiouAEIOU":
-    frasem = frase.replace(vocal.lower(), vocal.upper())
-    print("Frase con la vocal en mayúscula:", frasem)
-else:
-    print("La entrada de la vocal no es válida. Debe ser una sola vocal.")
