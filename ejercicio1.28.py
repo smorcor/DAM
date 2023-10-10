dd2156fcd031dc2bbbe4aa4cd54fb823a29a2b6e
SergioMorales12 <smorcor744a@g.educaand.es>
2023-10-10 19:07:54 +0200

    A

--- ejercicio1.28.py
+++ ejercicio1.28.py
@@ -1,13 +1,26 @@
 #Inicio
-numero1 = int(input("Introduce un número entero: ")) #Escribe "Introduce un número entero: "
-numero2 = int(input("Introduce otro número entero: ")) #Escribe "Introduce otro número entero: "
+#Inicio
+#Escribe "Introduce un número entero: "
+#Escribe "Introduce otro número entero: "
 
-if numero1 < numero2 : #Si (numero1<numero2) entonces
-    diferencia = numero2 - numero1 #diferencia = numero2 - numero1
-    print(f"El número menor es el {numero1} y entre ellos existen {diferencia} números enteros") #Escribe "El número menor es el "+numero1+" y entre ellos existen "+diferencia+" números enteros"
-elif numero1 > numero2 : #Sino (numero1 > numero2) entonces
-    diferencia2 = numero1 - numero2 #diferencia2 = numero1 - numero2
-    print(f"El número menor es el {numero2} y entre ellos existen {diferencia2} números enteros")#Escribe "El número menor es el "+numero2+" y entre ellos existen "+diferencia2+" números enteros"
-else : #Sino 
-    print("Los números no pueden ser iguales")#Escribe "Los números no pueden ser iguales"
+#Si (numero1<numero2) entonces
+    #diferencia = numero2 - numero1
+    #Escribe "El número menor es el "+numero1+" y entre ellos existen "+diferencia+" números enteros"
+#Sino (numero1 > numero2) entonces
+    #diferencia2 = numero1 - numero2
+    #Escribe "El número menor es el "+numero2+" y entre ellos existen "+diferencia2+" números enteros"
+#Sino 
+    #Escribe "Los números no pueden ser iguales"
 #Fin
+numero1 = int(input("Introduce un número entero: ")) 
+numero2 = int(input("Introduce otro número entero: "))
+
+if numero1 < numero2 : 
+    diferencia = numero2 - numero1 
+    print(f"El número menor es el {numero1} y entre ellos existen {diferencia} números enteros")
+elif numero1 > numero2 : 
+    diferencia2 = numero1 - numero2 
+    print(f"El número menor es el {numero2} y entre ellos existen {diferencia2} números enteros")
+else :  
+    print("Los números no pueden ser iguales")
+    
--- ejercicio1.29.py
+++ ejercicio1.29.py
@@ -1,4 +1,27 @@
-nombre = input("Introduce tu nombre: ")
-edad = input("Introduce tu edad: ")
 
-if nombre 
+#Inicio
+#Escribe "Introduce tu nombre: "
+#Escribe "Introduce tu edad: "
+
+#Mientras (nombre esta vacio) hacer 
+#   Lee "Desconocido"
+#Mientras (edad <= 125) y  (edad >= 0) hacer 
+#   Escribe "Introduce tu edad: "
+
+
+#Si (125 - edad >= 0) entonces
+#años = 125 - edad
+
+#Fin
+nombre = input("Introduce tu nombre: ") 
+edad = input("Introduce tu edad: ") 
+
+while (edad <= 125) and  (edad >= 0) : 
+    edad = input("Introduce tu edad: ") 
+
+
+if 125 - edad >= 0 and nombre = str: 
+    años = 125 - edad 
+    print(f"Te llamas {nombre} y tienes {edad} años, te quedan aún {años} años por cumplir") 
+else :
+    print("Desconocido")
