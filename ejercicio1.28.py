2457c621807fca5386e7c8981e3950969ab26c62
SergioMorales12 <smorcor744a@g.educaand.es>
2023-10-10 19:36:46 +0200

    Perfe

--- ejercicio1.28.py
+++ ejercicio1.28.py
@@ -6,13 +6,13 @@ Inicio
     Lee numero2
 
     Si (numero1<numero2) entonces
-        #diferencia = numero2 - numero1
-        #Escribe "El número menor es el "+numero1+" y entre ellos existen "+diferencia+" números enteros"
+        diferencia = numero2 - numero1
+        Escribe "El número menor es el "+numero1+" y entre ellos existen "+diferencia+" números enteros"
     Sino (numero1 > numero2) entonces
-        #diferencia2 = numero1 - numero2
-        #Escribe "El número menor es el "+numero2+" y entre ellos existen "+diferencia2+" números enteros"
+        diferencia2 = numero1 - numero2
+        Escribe "El número menor es el "+numero2+" y entre ellos existen "+diferencia2+" números enteros"
     Sino 
-        #Escribe "Los números no pueden ser iguales"
+        Escribe "Los números no pueden ser iguales"
 Fin
 """
 numero1 = int(input("Introduce un número entero: ")) 
