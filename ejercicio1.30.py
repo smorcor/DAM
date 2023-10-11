1855eb53f2dbd818589e7b066fc59c099cfff125
SergioMorales12 <smorcor744a@g.educaand.es>
2023-10-11 19:13:53 +0200

    actualizado

--- ejercicio1.30.py
+++ ejercicio1.30.py
@@ -1,33 +1,53 @@
-#Inicio
-#Escribe "Introduce un número de inicio: "
-#Escribe "Introduce un incremento: "
-#Escribe "Introduce un total de la serie: "
-
-#Si (incremento y total > 0) entonces
-#   
-#Fin
+"""
+Inicio
+    Escribe "Introduce un número de inicio: "
+    lee ini
+    Escribe "Introduce un incremento: "
+    lee inc
+    Escribe "Introduce un total de la serie: "
+    lee total
+    mientras (inc <= 0) o (total <= 0) hacer
+        lee inc
+        lee total
+    serie = 0
+    serie = "Serie => " + ini + "-"
+    cont = 1
+    mientras cont < total hacer
+        ini = ini + inc
+        cont = cont + 1
+        si cont < (total - 1 ) entonces
+            serie = serie + ini + ".."
+        sino 
+            si cont = total -1 entonces 
+                serie = serie + ini
+            sino
+                serie = serie + "-" + ini
+    lee serie
+Fin
+"""
 
 ini = int(input("Introduce un número de inicio: "))
 inc = int(input("Introduce un incremento: "))
 total = int(input("Introduce un total de la serie: "))
 
 while (inc <= 0) or (total <= 0) :
-    inc = input("Introduce un incremento mayor que 0: ")
-    total = input("Introduce un total de la serie que sea mayor que 0: ")
-serie= 0
-serie = f"Serie => {ini}"
+    inc = int(input("Introduce un incremento mayor que 0: "))
+    total = int(input("Introduce un total de la serie que sea mayor que 0: "))
+serie = 0
+serie = "Serie => " + str(ini) + "-"
 cont = 1
-while cont <= total :
-    ini = ini + inc
-    cont = cont + 1
-    if cont < (total - 1) :
-        serie = str(serie + ini + "..")
+while cont < total :
+    ini += inc
+    cont += 1
+
+    if cont < (total - 1 ) :
+        serie = serie + str(ini) + ".."
     else :
-        if cont == total :
-            serie = serie + ini
+        if cont == total -1:
+            serie = serie + str(ini)
         else : 
-            serie = serie + ini + "-"
+            serie = serie + "-" + str(ini)
 
-
+print(serie)
 
 
